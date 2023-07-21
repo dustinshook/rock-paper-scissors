259b5b439250eb8f27ccd5ffeb7985fa031d105a
dustin <dustin@fantasycruncher.com>
2023-07-21 17:27:22 -0400

    sounds & media queries

--- main.js
+++ main.js
@@ -47,22 +47,33 @@ function playRound(playerSelection, computerSelection) {
 const rps_game = () => {
     const baseElement = document.querySelector('#game');
     const scoreBoard = baseElement.querySelector('#scoreboard');
-    const resultMessage = baseElement.querySelector('#result-message');
+    const resultMessage = baseElement.querySelector('#action-message');
     const endGameElement = document.querySelector('#game-over');
 
     const home_score = scoreBoard.querySelector('#computer-score');
     const away_score = scoreBoard.querySelector('#user-score');
 
+    // audio
+    const win_sound = document.getElementById('game-win-sound');
+    const lose_sound = document.getElementById('game-lose-sound');
+    const tie_sound = document.getElementById('game-tie-sound');
+    const game_over_sound = document.getElementById('game-over-sound');
+    const game_over_sound_win = document.getElementById('game-over-win-sound');
+
     const messageReset = () => {
-        resultMessage.textContent = "";
+        const dots = document.createElement('span');
+        dots.classList.add('dots');
+        dots.textContent = '..';
+
+        resultMessage.textContent = "Choose your fighter";
+
+        resultMessage.appendChild(dots);
+        
     };
 
     const displayResultMessage = (result) => {
-        const resultElement = document.createElement('h1');
-        resultElement.textContent = result;
-        resultElement.classList.toggle('result');
-
-        resultMessage.appendChild(resultElement);
+        resultMessage.textContent = result;
+        resultMessage.classList.toggle('result');
     };
 
     const displayChoiceIcon = (choice, player) => {
@@ -87,7 +98,7 @@ const rps_game = () => {
         const delegatedElement = e.target.closest('div');
 
         if (choices.includes(delegatedElement?.id)) {
-            messageReset();
+            
             const playerSelection = delegatedElement.id;
             displayChoiceIcon(playerSelection, 'user');
 
@@ -98,9 +109,13 @@ const rps_game = () => {
 
             displayResultMessage(result);
 
+            setTimeout(messageReset, 2000);
+
             if (result.includes('win')) {
                 away_score.textContent = parseInt(away_score.textContent) + 1;
                 away_score.classList.add('win');
+
+                win_sound.play();
 
                 setTimeout(() => {
                     away_score.classList.remove('win');
@@ -110,11 +125,14 @@ const rps_game = () => {
                 home_score.textContent = parseInt(home_score.textContent) + 1;
                 home_score.classList.add('win');
 
+                lose_sound.play();
+
                 setTimeout(() => {
                     home_score.classList.remove('win');
                 }, 1000);
 
             } else {
+                tie_sound.play();
                 return;
             }
 
@@ -125,16 +143,19 @@ const rps_game = () => {
                 baseElement.classList.toggle('hidden');
                 endGameElement.classList.toggle('hidden');
 
+                game_over_sound.play();
+
                 endGameElement.querySelector('#game-over-message').textContent = `You lost! ${away_score_count} - ${home_score_count}`;
             } else if (away_score.textContent === '5') {
                 baseElement.classList.toggle('hidden');
                 endGameElement.classList.toggle('hidden');
 
+                game_over_sound_win.play();
+
                 endGameElement.querySelector('#game-over-message').textContent = `You won! ${away_score_count} - ${home_score_count}`;
             } else {
                 return;
             }
-
         }
     });
 
@@ -142,11 +163,14 @@ const rps_game = () => {
 
 const landingPage = document.querySelector('#landing');
 const restartButton = document.querySelector('#restart-game');
+const gameStartSound = document.getElementById('game-start-sound');
 
 landingPage.addEventListener('click', (e) => {
     if (e.target.id === 'start-game') {
         landingPage.classList.toggle('hidden');
         document.querySelector('#game').classList.toggle('hidden');
+
+        gameStartSound.play();
         rps_game();
     }
 });
